0744de354eb309650e09ef213cd81f178946f705
“ewhite1999” <ewhite1999@gmail.com>
2021-11-12 13:03:19 +0000

    Grid child coloured when hovered over

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const gridSize = 16 * 16;
+const gridSize = 16*16;
 const gridContainer = document.getElementById('grid_container');
 for(let i = 0; i < gridSize; i++){
     const newDiv = document.createElement('div');
     newDiv.className = 'grid_child';
     gridContainer.appendChild(newDiv);
 }
+
+const gridChild = document.querySelectorAll('.grid_child');
+gridChild.forEach(child => {
+    child.addEventListener("mouseover", function(){
+        child.classList.add('passed');
+    })
+})
